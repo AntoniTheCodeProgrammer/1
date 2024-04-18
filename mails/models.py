0c504bbe97e8dbfd5fe32a594275667d5dc745f5
AntoniTheCodeProgrammer <antonizmujdzin@gmail.com>
2024-04-18 16:49:58 +0200

    post tutorial

--- mails/models.py
+++ mails/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Mail(models.Model):
+    author = models.CharField(max_length=255)
+    text = models.CharField(max_length=2550)
--- mails/views.py
+++ mails/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 def mails(request):
-    return HttpResponse("Mails:")
+    template = loader.get_template('first.html')
+    return HttpResponse(template.render())
