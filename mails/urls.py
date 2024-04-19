ee38e44f08ab95269fb35cde71b39ed022800ca3
AntoniTheCodeProgrammer <antonizmujdzin@gmail.com>
2024-04-19 18:57:16 +0200

    details view

--- mails/urls.py
+++ mails/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.mails, name='mails'),
+    path('details/<int:id>', views.details, name='details'),
 ]
--- mails/views.py
+++ mails/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Campaign
 
 
 def mails(request):
-    template = loader.get_template('first.html')
-    return HttpResponse(template.render())
+    campaigns = Campaign.objects.all().values()
+    template1 = loader.get_template('first.html')
+    template = loader.get_template('campaigns.html')
+    context = {
+        'campaigns': campaigns, 
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  mycampaign = Campaign.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mycampaign': mycampaign,
+  }
+  return HttpResponse(template.render(context, request))
