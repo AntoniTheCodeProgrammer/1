1e03140addad955971386370d8c2e4de4a864bd0
AntoniTheCodeProgrammer <antonizmujdzin@gmail.com>
2024-04-24 00:24:32 +0200

    logowanie troche nie dziala

--- mails/urls.py
+++ mails/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from .views import authView
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('campaigns/', views.mails, name='mails'),
     path('campaigns/details/<slug:slug>', views.details, name='details'),
+    path('signup/', authView, name="authView"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- mails/views.py
+++ mails/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Campaign
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
+def main(request):
+    campaign = Campaign.objects.all().order_by('-replied').values()[0]
+    successprocent = []
+    # for campaign in campaigns:
+    #     successprocent.append((campaign[4]/campaign[6])*100)
+
+    # bestcampaign = successprocent.index(max(successprocent))
+    context = {
+        'mycampaign': campaign,
+    }
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render(context, request))
 
 def mails(request):
     campaigns = Campaign.objects.all()
-    template1 = loader.get_template('first.html')
     template = loader.get_template('campaigns.html')
     context = {
         'campaigns': campaigns, 
@@ -21,15 +35,16 @@ def details(request, slug):
     }
     return HttpResponse(template.render(context, request))
 
-def main(request):
-    campaign = Campaign.objects.all().order_by('-replied').values()[0]
-    successprocent = []
-    # for campaign in campaigns:
-    #     successprocent.append((campaign[4]/campaign[6])*100)
 
-    # bestcampaign = successprocent.index(max(successprocent))
+def authView(request):
+    if request.thod == "POST":
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
     context = {
-        'mycampaign': campaign,
+        'form' : form,
     }
-    template = loader.get_template('main.html')
+    template = loader.get_template('signup.html')
     return HttpResponse(template.render(context, request))
